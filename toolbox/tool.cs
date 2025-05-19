d045ca9f317c1a16686cca3104253da3101512ea
solhope23 <s526413103@gmail.com>
2025-05-19 16:45:16 +0300

    tool class

---
+++ toolbox/tool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+abstract public class tool
+{
+    public string Name;
+    public int Weight;
+
+    public tool(string name, int weight)
+    {
+        this.Name = name;
+        this.Weight = weight;
+    }
+
+    abstract public void Describe();
+
+    public void Use()
+    {
+        Console.WriteLine(Name + "is being used");
+    }
+}
